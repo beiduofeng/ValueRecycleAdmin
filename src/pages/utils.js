520672a62993d3d643f0b42702d728d5ddfce9c5
孙金瑶 <1323969103@qq.com>
2024-05-25 17:32:31 +0800

    密码验证

--- src/pages/utils.js
+++ src/pages/utils.js
@@ -1,12 +1,19 @@
 export function checkAccountPassword(account, password) {
     //return account == 111111 && password == 222222 ? true : false
-    if (account != 111111) {
+    console.log(account, password);
+    const accounts = [
+        { account: "111111", password: "121212" },
+        { account: "222222", password: "111111" },
+        { account: "333333", password: "313131" }
+    ];
+    let MathAccount = accounts.find(item => item.account === account)
+    if (!MathAccount) {
         return {
             type: "error",
             message: "登录失败，请重新输入账号"
         }
     } else {
-        if (password != 222222) {
+        if (MathAccount.password !== password) {
             return {
                 type: "error",
                 message: "登录失败，请重新输入密码"
