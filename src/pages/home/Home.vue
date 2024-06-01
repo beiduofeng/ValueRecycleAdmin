7f825bbdaf8fa348bb88fb2c1b9a3e769115e9fd
孙金瑶 <1323969103@qq.com>
2024-06-01 15:47:04 +0800

    home

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,12 +2,15 @@
 import { reactive, ref } from 'vue'
 const ruleFormRef = ref()
 import { ElMessage } from 'element-plus'
-
+import worker from './Worker.vue'
+import order from './Order.vue'
 </script>
 
 <template>
     <div class="home">
         {{ "home" }}
+        <staff />
+        <order />
     </div>
 </template>
 
--- src/router.js
+++ src/router.js
@@ -1,11 +1,15 @@
 import { createWebHashHistory, createRouter } from 'vue-router'
 
 import Login from './pages/Login.vue';
-import Home from './pages/Home.vue';
+import Home from './pages/home/Home.vue';
+import Order from './pages/home/Order.vue';
+import Worker from './pages/home/Worker.vue';
 
 const routes = [
     { path: '/', component: Home },
     { path: '/login', component: Login },
+    { path: '/worker', component: Worker },
+    { path: '/order', component: Order },
 ];
 
 const router = createRouter({
